e0eee549632f5b9005d020d02dbbf19a5feb8b73
n-prokofieva <n_prokofieva@icloud.com>
2023-02-15 00:11:48 -0500

    priceOfMangoes

---
+++ priceOfMangoes.js
@@ -0,0 +1,6 @@
+function mango(quantity, price){
+    return quantity*price - Math.floor(quantity/3)*price;
+}
+// mango(2, 3) ==> 6    # 2 mangoes for $3 per unit = $6; no mango for free
+// mango(3, 3) ==> 6    # 2 mangoes for $3 per unit = $6; +1 mango for free
+
