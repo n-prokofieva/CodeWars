d697e4226a81df153ac0e36f881ce77b9c4fdf84
n-prokofieva <n_prokofieva@icloud.com>
2023-02-15 00:11:21 -0500

    geometryBasicsDistanceBetweenPointsIn2D

---
+++ geometryBasicsDistanceBetweenPointsIn2D.js
@@ -0,0 +1,14 @@
+function distanceBetweenPoints(a, b) {
+    return Math.hypot(a.x - b.x, a.y - b.y);
+}
+
+// class Point1 {
+//   constructor(x, y) {
+//     this.x = x;
+//     this.y = y;
+//   }
+// }
+
+// const point = new Point1(3, 3);
+
+// console.log(point.x, point.y)
